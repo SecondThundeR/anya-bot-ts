9abed52468feb19f4b980f7e5e605feb56abe29c
SecondThundeR <awayfromgalaxy@gmail.com>
2022-09-05 22:06:42 +0300

    Update startMessage command
    - Add doc
    - Send new message for creator

--- src/commands/pm/startMessage/startMessage.ts
+++ src/commands/pm/startMessage/startMessage.ts
@@ -1,11 +1,18 @@
 import { Composer } from 'grammy';
 import otherMessages from '../../../locale/otherMessages';
+import RegularUtils from '../../../utils/regularUtils';
 
 const startMessage = new Composer();
 
-startMessage.command(
-    'start',
-    async ctx => await ctx.reply(otherMessages.noPMHint)
-);
+/**
+ * This command is used to set start message for bot
+ * When command triggered by non-creator user, bot will send dummy placeholder.
+ * Otherwise, send note to use /help for more info
+ */
+startMessage.command('start', async ctx => {
+    if (!RegularUtils.isBotCreator(ctx))
+        await ctx.reply(otherMessages.noPMHint);
+    await ctx.reply(otherMessages.creatorPMHint);
+});
 
 export default startMessage;
