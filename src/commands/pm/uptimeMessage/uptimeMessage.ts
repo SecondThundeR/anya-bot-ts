998bc9c8275c755654abf442d967d39dc267084d
SecondThundeR <awayfromgalaxy@gmail.com>
2022-09-10 14:07:04 +0300

    Fix check for creator in /uptime command

--- src/commands/pm/uptimeMessage/uptimeMessage.ts
+++ src/commands/pm/uptimeMessage/uptimeMessage.ts
@@ -5,10 +5,10 @@ import RegularUtils from '../../../utils/regularUtils';
 const uptimeMessage = new Composer();
 
 uptimeMessage.command('uptime', async ctx => {
-    if (!RegularUtils.isBotCreator(ctx))
-        return await ctx.reply(getUptimeMessage(), {
-            parse_mode: 'HTML'
-        });
+    if (!RegularUtils.isBotCreator(ctx)) return;
+    await ctx.reply(getUptimeMessage(), {
+        parse_mode: 'HTML'
+    });
 });
 
 export default uptimeMessage;
