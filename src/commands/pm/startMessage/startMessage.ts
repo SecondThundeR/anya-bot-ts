5cbe197521c1e160a092dc352463b4e1e0f0a0e1
SecondThundeR <awayfromgalaxy@gmail.com>
2022-09-10 13:58:33 +0300

    Remove startMessage.ts docs
    Not showing up in IDE, so I guess this is not needed right now

--- src/commands/pm/startMessage/startMessage.ts
+++ src/commands/pm/startMessage/startMessage.ts
@@ -4,11 +4,6 @@ import RegularUtils from '../../../utils/regularUtils';
 
 const startMessage = new Composer();
 
-/**
- * This command is used to set start message for bot
- * When command triggered by non-creator user, bot will send dummy placeholder.
- * Otherwise, send note to use /help for more info
- */
 startMessage.command('start', async ctx => {
     if (!RegularUtils.isBotCreator(ctx))
         return await ctx.reply(otherMessages.noPMHint);
