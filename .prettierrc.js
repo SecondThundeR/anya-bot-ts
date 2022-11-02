17f05fcfa6e5f73a254b439b64b536dd8f0034a0
SecondThundeR <awayfromgalaxy@gmail.com>
2022-11-02 11:17:59 +0300

    feat(.prettier.js): add new import order paths

--- .prettierrc.js
+++ .prettierrc.js
@@ -22,6 +22,8 @@ module.exports = {
         '^@pmCommands/(.*)$',
         '^@groupHandlers/(.*)$',
         '^@pmHandlers/(.*)$',
+        '^@enums/(.*)$',
+        '^@locale/(.*)$',
         '^@utils/(.*)$',
         '^[./]'
     ],
