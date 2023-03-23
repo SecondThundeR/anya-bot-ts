5a2bc446a60c6a72f0880e3dfd7db45a3248a377
SecondThundeR <awayfromgalaxy@gmail.com>
2023-03-24 01:41:43 +0300

    fix: update deps.ts
    node's process is now built-in
    dotenv's config method was renamed to load

--- deps.ts
+++ deps.ts
@@ -19,5 +19,4 @@ export type {
 export { run, sequentialize } from 'grammy_runner/mod.ts';
 export { createLazyClient } from 'redis/mod.ts';
 export type { Redis } from 'redis/mod.ts';
-export { config as dotenv } from 'dotenv/mod.ts';
-export { process } from 'node/process.ts';
+export { load as dotenv } from 'dotenv/mod.ts';
