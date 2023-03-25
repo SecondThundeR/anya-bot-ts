00d3af39412321953355f5e238cd27040aa168a4
SecondThundeR <awayfromgalaxy@gmail.com>
2023-03-25 22:54:50 +0300

    fix(addWhiteList): reorder statements

--- src/commands/pm/addWhiteList/index.ts
+++ src/commands/pm/addWhiteList/index.ts
@@ -16,41 +16,44 @@ addWhiteList.command("addwhitelist", async ctx => {
     const chatID = ctx.match;
     const idsLists = await redisInstance.getLists([
         ListsNames.WHITELIST,
-        ListsNames.IGNORELIST
+        ListsNames.IGNORELIST,
     ]);
 
     if (!RegularUtils.isBotCreator(ctx)) return;
 
-    if (RegularUtils.isStringEmpty(chatID))
+    if (RegularUtils.isStringEmpty(chatID)) {
         return await ctx.reply(otherMessages.noChatIDProvided);
+    }
 
-    if (RegularUtils.isItemInList(chatID, idsLists[ListsNames.WHITELIST]))
+    if (RegularUtils.isItemInList(chatID, idsLists[ListsNames.WHITELIST])) {
         return await ctx.reply(whiteListMessages.alreadyAdded);
+    }
 
     await RedisSingleton.getInstance().pushValueToList(
         ListsNames.WHITELIST,
-        String(chatID)
+        String(chatID),
     );
 
     if (RegularUtils.isItemInList(chatID, idsLists[ListsNames.IGNORELIST])) {
         await RedisSingleton.getInstance().removeValueFromList(
             ListsNames.IGNORELIST,
-            String(chatID)
+            String(chatID),
         );
         return await ctx.reply(whiteListMessages.addedAndUnignored);
     }
+    await ctx.reply(whiteListMessages.added);
 
     const isInChat = await AsyncUtils.isBotInChat(ctx, chatID);
-    if (isInChat) await AsyncUtils.sendAccessGrantedMessage(ctx, chatID);
+    if (!isInChat) return;
 
+    await AsyncUtils.sendAccessGrantedMessage(ctx, chatID);
     const botData = await ctx.api.getChatMember(chatID, ctx.me.id);
-    if (!RegularUtils.isBotCanDelete(botData))
-        await ctx.api.sendMessage(
-            chatID,
-            otherMessages.botAdminWhitelistedHint
-        );
+    if (!RegularUtils.isBotCanDelete(botData)) return;
 
-    await ctx.reply(whiteListMessages.added);
+    await ctx.api.sendMessage(
+        chatID,
+        otherMessages.botAdminWhitelistedHint,
+    );
 });
 
 export default addWhiteList;
