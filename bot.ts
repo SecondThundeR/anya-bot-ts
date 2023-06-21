c60facd218bfbaa7d0d3c208110fa3499e4fb377
SecondThundeR <awayfromgalaxy@gmail.com>
2023-06-21 15:15:44 +0300

    refactor: update bot.ts
    Add new commands and handlers
    Add conversation plugin support
    Refactor logic

--- bot.ts
+++ bot.ts
@@ -1,16 +1,19 @@
 import {
     Bot,
+    conversations,
+    createConversation,
     dotenv,
     GrammyError,
     HttpError,
     run,
+    RunnerHandle,
     sequentialize,
     session,
 } from "@/deps.ts";
 // @deno-types="npm:@types/node"
 import process from "node:process";
 
-import otherMessages from "@/locales/otherMessages.ts";
+import importDatabaseConversation from "@/conversations/importDatabase.ts";
 
 import adminPowerTrigger from "@/groupCommands/adminPowerTrigger/index.ts";
 import aidenMode from "@/groupCommands/aidenMode/index.ts";
@@ -28,129 +31,160 @@ import silentTrigger from "@/groupCommands/silentTrigger/index.ts";
 
 import addIgnoreList from "@/pmCommands/addIgnoreList/index.ts";
 import addWhiteList from "@/pmCommands/addWhiteList/index.ts";
+import exportDatabase from "@/pmCommands/exportDatabase/index.ts";
 import getCommandsUsage from "@/pmCommands/getCommandsUsage/index.ts";
 import getIgnoreList from "@/pmCommands/getIgnoreList/index.ts";
 import getWhiteList from "@/pmCommands/getWhiteList/index.ts";
 import helpPMMessage from "@/pmCommands/helpPMMessage/index.ts";
+import importDatabase from "@/pmCommands/importDatabase/index.ts";
 import removeIgnoreList from "@/pmCommands/removeIgnoreList/index.ts";
 import removeWhiteList from "@/pmCommands/removeWhiteList/index.ts";
 import startMessage from "@/pmCommands/startMessage/index.ts";
-import uptimeMessage from "@/pmCommands/uptimeMessage/index.ts";
+import uptimeCommand from "@/pmCommands/uptimeCommand/index.ts";
 
 import customEmojisHandler from "@/groupHandlers/customEmojisHandler/index.ts";
 import groupCallbackHandler from "@/groupHandlers/groupCallbackHandler/index.ts";
+import inlineNicknameGenerator from "@/groupHandlers/inlineNicknameGenerator/index.ts";
 import newChatHandler from "@/groupHandlers/newChatHandler/index.ts";
 import premiumStickersHandler from "@/groupHandlers/premiumStickersHandler/index.ts";
 import voiceAndVideoHandler from "@/groupHandlers/voiceAndVideoHandler/index.ts";
 
 import pmCallbackHandler from "@/pmHandlers/pmCallbackHandler/index.ts";
 
-import AsyncUtils from "@/utils/asyncUtils.ts";
-import RedisSingleton from "@/database/redisSingleton.ts";
-import RegularUtils from "@/utils/regularUtils.ts";
+import redisClient from "@/database/redisClient.ts";
+
+import otherMessages from "@/locales/otherMessages.ts";
+
+import checkForAdminCommand from "@/middlewares/group/checkForAdminCommand.ts";
+import checkForWhitelist from "@/middlewares/group/checkForWhitelist.ts";
+import incrementCommandUsage from "@/middlewares/group/incrementCommandUsage.ts";
+import checkForCreatorID from "@/middlewares/pm/checkForCreatorID.ts";
+
+import BotContext from "@/types/context.ts";
+
+import { getMessageID, getSessionKey } from "@/utils/apiUtils.ts";
+import { logBotInfo } from "@/utils/asyncUtils.ts";
+import { setPlaceholderData } from "@/utils/generalUtils.ts";
 
 await dotenv({ export: true, allowEmptyValues: true });
 
 const BOT_TOKEN = Deno.env.get("BOT_TOKEN");
-
 if (!BOT_TOKEN) {
-    console.error(
-        "WARNING: Token for bot is not provided. Please set the BOT_TOKEN environment variable.",
+    throw new Error(
+        "Token for bot wasn't provided. Please set the BOT_TOKEN environment variable.",
     );
-    Deno.exit(1);
 }
 
-const bot = new Bot(BOT_TOKEN);
-const runner = run(bot);
-const client = RedisSingleton.getInstance();
+const bot = new Bot<BotContext>(BOT_TOKEN);
+let runner: RunnerHandle | undefined;
+
+bot.use(session({
+    initial: () => ({}),
+    getSessionKey,
+}));
+bot.use(sequentialize(getSessionKey));
+bot.use(conversations());
+bot.use(createConversation(importDatabaseConversation, "import"));
 
 const pm = bot.filter((ctx) => ctx.chat?.type === "private");
 const group = bot.filter(
     (ctx) => ctx.chat?.type !== "private" && ctx.chat?.type !== "channel",
 );
 
-const getSessionKeyFunc = RegularUtils.getSessionKey;
-const stopOnTerm = async () => {
-    if (runner.isRunning()) {
-        await runner.stop();
-        await client.quitClient();
-        return true;
-    }
-    return false;
-};
-
-bot.use(sequentialize(getSessionKeyFunc));
-// @ts-ignore
-bot.use(session({ getSessionKeyFunc }));
+// Commands Middlewares
+group.use(checkForWhitelist);
+group.use(checkForAdminCommand);
+group.use(incrementCommandUsage);
+pm.use(checkForCreatorID);
 
 // Group Commands
-group.use(helpGroupMessage);
+group.use(adminPowerTrigger);
 group.use(aidenMode);
-group.use(silentTrigger);
 group.use(aidenSilentTrigger);
 group.use(diceGame);
-group.use(adminPowerTrigger);
-group.use(silentOnLocale);
-group.use(silentOnLocaleReset);
-group.use(silentOffLocale);
-group.use(silentOffLocaleReset);
+group.use(helpGroupMessage);
 group.use(messageLocale);
 group.use(messageLocaleReset);
 group.use(noCustomEmoji);
+group.use(silentOffLocale);
+group.use(silentOffLocaleReset);
+group.use(silentOnLocale);
+group.use(silentOnLocaleReset);
+group.use(silentTrigger);
 
 // Group Handlers
 group.use(groupCallbackHandler);
-group.use(newChatHandler);
 group.use(customEmojisHandler);
+group.use(inlineNicknameGenerator);
+group.use(newChatHandler);
 group.use(premiumStickersHandler);
 group.use(voiceAndVideoHandler);
 
 // PM Commands
-pm.use(startMessage);
-pm.use(helpPMMessage);
-pm.use(addWhiteList);
-pm.use(removeWhiteList);
-pm.use(getWhiteList);
 pm.use(addIgnoreList);
-pm.use(removeIgnoreList);
-pm.use(getIgnoreList);
-pm.use(uptimeMessage);
+pm.use(addWhiteList);
+pm.use(exportDatabase);
 pm.use(getCommandsUsage);
+pm.use(getIgnoreList);
+pm.use(getWhiteList);
+pm.use(helpPMMessage);
+pm.use(importDatabase);
+pm.use(removeIgnoreList);
+pm.use(removeWhiteList);
+pm.use(startMessage);
+pm.use(uptimeCommand);
 
 // PM Handlers
 pm.use(pmCallbackHandler);
 
 bot.catch(async (err) => {
-    const ctx = err.ctx;
-    console.error(`Error while handling update ${ctx.update.update_id}:`);
-    const e = err.error;
-    if (e instanceof GrammyError) {
-        return console.error("Error in request:", e.description);
+    const { ctx, error, ctx: { message } } = err;
+
+    console.error(
+        `Error while handling update: ${JSON.stringify(ctx.update, null, 4)}`,
+    );
+
+    if (error instanceof GrammyError) {
+        console.error("Error in request:", error.description);
+        return;
     }
-    if (e instanceof HttpError) {
-        return console.error("Could not contact Telegram:", e);
+
+    if (error instanceof HttpError) {
+        console.error("Could not contact Telegram:", error);
+        return;
     }
-    if (err.ctx.message) {
+
+    if (message) {
         await bot.api.sendMessage(
-            err.ctx.message?.chat.id,
-            RegularUtils.setPlaceholderData(otherMessages.unknownError, {
-                error: String(e),
+            message?.chat.id,
+            setPlaceholderData(otherMessages.unknownError, {
+                error: String(error),
             }),
             {
-                reply_to_message_id: RegularUtils.getMessageID(err.ctx.message),
+                reply_to_message_id: getMessageID(message),
                 parse_mode: "HTML",
             },
         );
     }
-    return console.error("Unknown error occurred:", e);
+    console.error("Unknown error occurred:", error);
 });
+
+const stopOnTerm = async () => {
+    if (runner?.isRunning()) {
+        await runner.stop();
+        await redisClient.quitClient();
+        return true;
+    }
+    return false;
+};
 
 process.once("SIGINT", stopOnTerm);
 process.once("SIGTERM", stopOnTerm);
 
 try {
-    await AsyncUtils.logBotInfo(bot.api);
+    runner = run(bot);
+    await logBotInfo(bot.api);
 } catch (e) {
     console.log(e);
-    await client.quitClient();
+    await redisClient.quitClient();
 }
