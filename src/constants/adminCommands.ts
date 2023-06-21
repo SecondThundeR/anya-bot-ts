a01315bcf8741e83c7c3e085981c1be9fcbaf926
SecondThundeR <awayfromgalaxy@gmail.com>
2023-06-21 15:14:21 +0300

    feat(constants): add relevant constants for middleware

---
+++ src/constants/adminCommands.ts
@@ -0,0 +1,19 @@
+/**
+ * Names of commands, that can be called by admins
+ */
+const AdminCommands = [
+    "/adminpower",
+    "/aidenmode",
+    "/aidensilent",
+    "/help",
+    "/messagelocale",
+    "/messagelocalereset",
+    "/noemoji",
+    "/silentofflocale",
+    "/silentofflocalereset",
+    "/silentonlocale",
+    "/silentonlocalereset",
+    "/silent",
+] as const;
+
+export default AdminCommands;
